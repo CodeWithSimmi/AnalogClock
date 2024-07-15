c304d43a767a2ac79dc4128def567a821ead625a
CodeWithSimmi <160124883+CodeWithSimmi@users.noreply.github.com>
2024-07-15 19:49:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const body = document.querySelector("body"),
+      hourHand = document.querySelector(".hand.hour"),
+      minuteHand = document.querySelector(".hand.minute"),
+      secondHand = document.querySelector(".hand.second"),
+      modeSwitch = document.querySelector(".mode-switch");
+
+
+      if(localStorage.getItem("mode") === "Dark Mode"){
+        body.classList.add("dark");
+        modeSwitch.textContent="Light Mode";
+      }
+
+
+      modeSwitch.addEventListener("click",() => {
+        body.classList.toggle("dark");
+
+        const isDarkMode =body.classList.contains("dark");
+
+        modeSwitch.textContent=isDarkMode ? "Light Mode" : "Dark Mode";
+
+        localStorage.setItem("mode", isDarkMode ? "Dark Mode" : "Light Mode");
+      });
+  
+    const updateTime = () => {
+      let date = new Date(),
+        secToDeg = (date.getSeconds() / 60) * 360;
+        minToDeg = (date.getMinutes() / 60) * 360;
+        hrToDeg = (date.getHours() / 12) * 360;
+  
+      secondHand.style.transform = `rotate(${secToDeg}deg)`;
+      minuteHand.style.transform = `rotate(${minToDeg }deg)`;
+      hourHand.style.transform = `rotate(${ hrToDeg}deg)`;
+    };
+  
+    setInterval(updateTime, 1000);
+  });
